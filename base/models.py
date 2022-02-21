1982a59d4c808be4ef154e109151ce4e81a00fc1
LukeTreece-HU <71277076+LukeTreece-HU@users.noreply.github.com>
2022-02-21 16:01:59 -0600

    CRUD

--- base/models.py
+++ base/models.py
@@ -12,10 +12,13 @@ class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
-    desciption = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     #participants = 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta: 
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.name
